79dbb5916ca30384f5e5b1cb8114c688a1ace990
jobscale <jobscale@users.noreply.github.com>
2023-04-22 22:44:15 +0900

    template updated

--- LambdaSchedule-examples/app/createAndRemoveAmi.js
+++ LambdaSchedule-examples/app/createAndRemoveAmi.js
@@ -1,6 +1,6 @@
 const { Ami } = require('./services/ami');
 
-const { env } = process;
+const { AMI_RETENTION_PERIOD } = process.env;
 
 const wait = ms => new Promise(resolve => { setTimeout(resolve, ms); });
 
@@ -9,7 +9,7 @@ const createAndRemoveAmi = () => {
   return ami.listInstances([{ Name: 'tag:Backup', Values: ['yes'] }])
   .then(instances => ami.createImages(instances))
   .then(() => wait(5000))
-  .then(() => ami.listExpiredImages(env.AMI_RETENTION_PERIOD))
+  .then(() => ami.listExpiredImages(AMI_RETENTION_PERIOD))
   .then(images => ami.deregisterImages(images))
   .then(mappings => ami.deleteSnapshots(mappings));
 };
