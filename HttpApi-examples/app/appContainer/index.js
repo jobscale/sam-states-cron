2b32db2e9e963e4d1239f4a36a48129261ee71b9
jobscale <jobscale@users.noreply.github.com>
2023-04-24 11:38:17 +0900

    Lambda updated

--- HttpApi-examples/app/appContainer/index.js
+++ HttpApi-examples/app/appContainer/index.js
@@ -1,30 +1,111 @@
-const logger = console;
+Error.stackTraceLimit = 20;
+const { StandardRetryStrategy } = require('@aws-sdk/middleware-retry');
+const {
+  DynamoDBClient,
+  PutItemCommand,
+  GetItemCommand,
+  UpdateItemCommand,
+  DeleteItemCommand,
+  QueryCommand,
+  ScanCommand,
+  ProvisionedThroughputExceededException,
+  DynamoDBClientConfig,
+} = require('@aws-sdk/client-dynamodb');
+const { marshall, unmarshall } = require('@aws-sdk/util-dynamodb');
 
-const main = async event => {
-  return { message: 'ok' };
-};
+const { ENV } = process.env;
+const logger = console;
+const TableName = `${ENV}-app-AutoscalingSample`;
+
+const maxAttempts = 20;
+const ddb = new DynamoDBClient({ maxAttempts, logger });
+
+const wait = async ms => new Promise(resolve => { setTimeout(resolve, ms); });
+const parse = async text => JSON.parse(
+  text[0] === '{' ? text : Buffer.from(text, 'base64').toString(),
+);
+
+class App {
+  async tryWrite({ prefix, count }) {
+    const bytes = 492;
+    const buffer = [];
+    for (let i = 0; i < bytes; i++) {
+      buffer.push(Math.floor(Math.random() * 0xff));
+    }
+    const body = `0x${Buffer.from(buffer).toString('hex')}`;
+    logger.info({ bytes: JSON.stringify(marshall({ uuid: 'nnnn', body })).length });
+
+    const pending = [];
+    for (let i = 0; i < count; i++) {
+      const uuid = `${prefix}-${i}`;
+      pending.push(
+        ddb.send(new PutItemCommand({ TableName, Item: marshall({ uuid, body }) })),
+      );
+    }
+    return Promise.allSettled(pending)
+    .then(settledList => {
+      const fulfilled = settledList.filter(settled => settled.status === 'fulfilled');
+      const succeeded = fulfilled.filter(settled => settled.value);
+      return {
+        Total: pending.length,
+        Fulfilled: fulfilled.length,
+        Succeeded: succeeded.length,
+        Bugs: fulfilled.length - succeeded.length,
+        Failed: pending.length - fulfilled.length,
+      };
+    });
+  }
+
+  async tryRead({ prefix, count }) {
+    const pending = [];
+    for (let i = 0; i < count; i++) {
+      const uuid = `${prefix}-${i}`;
+      pending.push(
+        ddb.send(new GetItemCommand({ TableName, Key: marshall({ uuid }) })),
+      );
+    }
+    return Promise.allSettled(pending)
+    .then(settledList => {
+      const fulfilled = settledList.filter(settled => settled.status === 'fulfilled');
+      const succeeded = fulfilled.filter(settled => settled.value);
+      return {
+        Total: pending.length,
+        Fulfilled: fulfilled.length,
+        Succeeded: succeeded.length,
+        Bugs: fulfilled.length - succeeded.length,
+        Failed: pending.length - fulfilled.length,
+      };
+    });
+  }
+
+  async main(event) {
+    const { type, prefix, count } = await parse(event.body ? event.body : event);
+    if (type === 'write') return this.tryWrite({ prefix, count });
+    if (type === 'read') return this.tryRead({ prefix, count });
+    throw new Error('no supported');
+  }
+}
 
 exports.handler = async event => {
-  logger.info('EVENT', JSON.stringify(event));
+  logger.info('EVENT', JSON.stringify(event, null, 2));
 
-  return main(event)
+  return new App().main(event)
   .then(res => {
     return {
       statusCode: 200,
-      headers: { 'Content-Length': 'application/json' },
+      headers: { 'Content-Type': 'application/json; charset=UTF-8' },
       body: JSON.stringify(res),
     };
   })
   .catch(e => {
-    logger.error(e);
     return {
       statusCode: 503,
-      headers: { 'Content-Length': 'application/json' },
-      body: JSON.stringify({ message: e.message }),
+      headers: { 'Content-Type': 'application/json; charset=UTF-8' },
+      body: JSON.stringify({ type: e.name, error: e.message }),
     };
   })
   .then(response => {
-    logger.info('RESPONSE', JSON.stringify(response));
+    logger.info('RESPONSE', JSON.stringify(response, null, 2));
     return response;
   });
 };
--- HttpApi-examples/app/appPath/index.js
+++ HttpApi-examples/app/appPath/index.js
@@ -1,16 +1,47 @@
+const { LambdaClient, InvokeCommand } = require('@aws-sdk/client-lambda');
+
 const logger = console;
+
+const { FunctionName } = process.env;
+const lambda = new LambdaClient();
+
+const invoke = async payload => {
+  return lambda.send(new InvokeCommand({
+    FunctionName,
+    Payload: JSON.stringify(payload),
+    InvocationType: 'Event',
+    LogType: 'Tail',
+  }));
+};
+
+const main = async event => {
+  return {
+    ts: Date.now(),
+    ...await invoke(event.body),
+  };
+};
+
 exports.handler = async event => {
   logger.info('EVENT', JSON.stringify(event, null, 2));
-  const response = {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
-    body: JSON.stringify({
-      date: new Date(),
-      message: 'ok',
-    }),
-  };
-  logger.info('RESPONSE', JSON.stringify(response, null, 2));
-  return response;
+
+  return main(event)
+  .then(res => {
+    return {
+      statusCode: 200,
+      headers: { 'Content-Type': 'application/json; charset=UTF-8' },
+      body: JSON.stringify(res),
+    };
+  })
+  .catch(e => {
+    logger.error(e);
+    return {
+      statusCode: e.statusCode || 503,
+      headers: { 'Content-Type': 'application/json; charset=UTF-8' },
+      body: JSON.stringify({ error: e.name }),
+    };
+  })
+  .then(response => {
+    logger.info('RESPONSE', JSON.stringify(response, null, 2));
+    return response;
+  });
 };
